057ca3ae1198e01135fa1cf391a7c57bb450a21d
TaeYoon <tyk117@kookmin.ac.kr>
2024-04-03 18:17:25 +0900

    fix: AWL func argument error

--- GIPS/core/AWL.py
+++ GIPS/core/AWL.py
@@ -1,4 +1,4 @@
-from core.HH import DHH
+from core.HH import THH, DHH
 
 '''
 payloads: 화이트 리스트를 하기 위한 payloads
@@ -6,15 +6,18 @@ ngram, hh1_size, hh2_size, ratio: DHH 하이퍼 파라미터
 '''
 
 
-def AWL(payloads, ngram, hh1_size, hh2_size, ratio):
+def AWL(payloads, ngram, hh1_size, hh2_size, hh3_size, ratio):
 
-    stopwords = DHH(
+    print('start whtie list')
+    stopwords = THH(
         packets=payloads,
         k=ngram,
         hh1_size=hh1_size,
         hh2_size=hh2_size,
+        hh3_size=hh3_size,
         ratio=ratio,
         deduplication=True,
     )
+    print('end white list')
 
     return stopwords
