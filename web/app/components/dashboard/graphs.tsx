7eb2a66dd82f372ecc7cee2360017e20dd6513ee
TaeYoon <tyk117@kookmin.ac.kr>
2024-04-13 18:12:44 +0900

    fix: design improve

--- web/app/components/dashboard/graphs.tsx
+++ web/app/components/dashboard/graphs.tsx
@@ -1,52 +1,104 @@
-'use client'
+"use client";
 import dynamic from "next/dynamic";
-import React, { useEffect } from 'react';
-import ApexCharts from 'apexcharts';
+import React, { useEffect } from "react";
+import ApexCharts from "apexcharts";
 import { mockGraphData } from "../../dashboard/mockData/graph";
 
-const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
-const TrafficGraph:React.FC = () => {
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
-  const values:number[] = mockGraphData.map(mockGraphData => mockGraphData.count);
-  const labels:string[] = mockGraphData.map(mockGraphData => mockGraphData.type);
-    useEffect(() => {
-      const options = {
-        series: values,
-        labels: labels,
-        chart: {
-          type: 'pie',
+const TrafficGraph: React.FC = () => {
+  const values: number[] = mockGraphData.map(
+    (mockGraphData) => mockGraphData.count
+  );
+
+  const labels: string[] = mockGraphData.map(
+    (mockGraphData) => mockGraphData.type
+  );
+
+  useEffect(() => {
+    const options = {
+      series: values,
+      labels: labels,
+      chart: {
+        type: "pie",
+      },
+      title: {
+        show: false,
+      },
+      dataLabels: {
+        textAnchor: "middle",
+        style: {
+          colors: ["#fff", "#fff", "#1f2937"],
         },
-        stroke: {
-          colors: ['#fff']
+        dropShadow: {
+          enabled: false,
         },
-        fill: {
-          opacity: 0.8
+      },
+      plotOptions: {
+        pie: {
+          dataLabels: {
+            offset: -15,
+          },
         },
-        responsive: [{
+      },
+      legend: {
+        position: "bottom",
+      },
+      stroke: {
+        width: 4,
+        colors: ["rgb(255, 255, 255)"],
+      },
+      states: {
+        hover: {
+          filter: {
+            type: "none",
+          },
+        },
+      },
+      colors: ["#3b82f6", "#22d3ee", "#e5e7eb", "#a855f7"],
+      animations: {
+        enabled: true,
+        easing: "easeinout",
+        speed: 800,
+        animateGradually: {
+          enabled: true,
+          delay: 150,
+        },
+        dynamicAnimation: {
+          enabled: true,
+          speed: 350,
+        },
+      },
+      responsive: [
+        {
           breakpoint: 480,
           options: {
-            chart: {
-            },
+            chart: {},
             legend: {
-              position: 'bottom'
-            }
-          }
-        }]
-      };
-      
-      const chart = new ApexCharts(document.querySelector("#chart"), options);
+              position: "bottom",
+            },
+          },
+        },
+      ],
+    };
+
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
-  
-      // Cleanup
-      return () => {
-        chart.destroy();
-      };
-    }, []);
-  
+
+    // Cleanup
+    return () => {
+      chart.destroy();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-      <div id="chart" className="flex flex-col justify-center items-center h-3/4 w-3/4">
-      </div>
+    <div
+      id="chart"
+      className="flex flex-col justify-center items-center h-3/4 w-3/4"
+    ></div>
   );
 };
 
--- web/app/dashboard/mockData/graph.ts
+++ web/app/dashboard/mockData/graph.ts
@@ -2,19 +2,19 @@ import { TrafficGraphData } from "@customTypes/mock/dashboard";
 
 export const mockGraphData: TrafficGraphData[] = [
   {
-    type:'exe',
-    count:123
+    type: "exe",
+    count: 123,
   },
   {
-    type:'lib',
-    count:56
+    type: "lib",
+    count: 56,
   },
   {
-    type:'sys',
-    count:321
+    type: "sys",
+    count: 321,
   },
   {
-    type:'obj',
-    count:23
+    type: "obj",
+    count: 36,
   },
 ];
