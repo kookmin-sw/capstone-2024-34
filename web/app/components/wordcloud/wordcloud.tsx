223d89363d42464efc0f9704515f36da9e7a4c0c
TaeYoon <tyk117@kookmin.ac.kr>
2024-04-14 16:12:30 +0900

    fix: angle & overflow

--- web/app/components/wordcloud/wordcloud.tsx
+++ web/app/components/wordcloud/wordcloud.tsx
@@ -5,12 +5,19 @@ import ReactWordcloud from "react-wordcloud";
 
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
+
 import { mockWordCloudData } from "../../dashboard/mockData/words";
+import { MinMaxPair } from "react-wordcloud";
+
+const options = {
+  rotations: 1,
+  rotationAngles: [0.0, 0.0] as MinMaxPair,
+};
 
 const WordCloud = () => {
   return (
-    <div style={{ height: 400, width: 600 }}>
-      <ReactWordcloud words={mockWordCloudData} />
+    <div className="w-full">
+      <ReactWordcloud words={mockWordCloudData} options={options} />
     </div>
   );
 };
