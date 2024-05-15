10914803b879ba7c0de50725de48a14707521aeb
parkjs82 <54925443+parkjs82@users.noreply.github.com>
2024-05-15 16:36:50 +0900

    feat: get signature api

--- web/app/components/dashboard/wordcloud.tsx
+++ web/app/components/dashboard/wordcloud.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { WordCloudData } from "@customTypes/mock/wordcloud";
 import dynamic from "next/dynamic";
 
 const ReactWordcloud = dynamic(() => import("@cyberblast/react-wordcloud"), {
@@ -10,8 +11,7 @@ import { MinMaxPair } from "@cyberblast/react-wordcloud";
 
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
-
-import { mockWordCloudData } from "../../(page)/dashboard/mockData/words";
+import { fetchData } from "next-auth/client/_utils";
 
 const options = {
   rotations: 1,
@@ -19,11 +19,36 @@ const options = {
   fontFamily: "SUIT",
 };
 
-const WordCloud = ({
-  words = mockWordCloudData,
-  wordCloudoptions = options,
-}) => {
+const WordCloud = ({ wordCloudoptions = options }) => {
+  const [signature, setSignature] = useState<WordCloudData[]>([]);
   const [iteration, setIteration] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/signature", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await res.json();
+        let tmpSig: WordCloudData[] = [];
+        for (const element in data) {
+          const wordCloudItem: WordCloudData = {
+            text: data[element].signature,
+            value: Number(element),
+          };
+          tmpSig.push(wordCloudItem);
+        }
+        setSignature(tmpSig);
+      } catch (err) {
+        console.log("Failed to send request");
+      }
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -34,7 +59,7 @@ const WordCloud = ({
 
   return (
     <>
-      <ReactWordcloud words={words} options={wordCloudoptions} />
+      <ReactWordcloud words={signature} options={wordCloudoptions} />
     </>
   );
 };
