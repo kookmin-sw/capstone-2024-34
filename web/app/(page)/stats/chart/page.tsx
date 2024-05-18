a67f3d59f1fed91699029d22f38056b3415a1da1
TaeYoon <tyk117@kookmin.ac.kr>
2024-05-19 00:51:45 +0900

    add: wordcloud

--- web/app/(page)/stats/chart/page.tsx
+++ web/app/(page)/stats/chart/page.tsx
@@ -6,6 +6,7 @@ import ApexChart1 from "@components/chart/chart1";
 import AttackChart from "@components/chart/AttackChart";
 import SignatureChart from "@components/chart/SignatureChart";
 import TrafficGraph from "@components/dashboard/graphs";
+import WordCloud from "@components/dashboard/wordcloud";
 
 export default function ChartPage() {
   return (
@@ -57,6 +58,17 @@ export default function ChartPage() {
             </div>
             <div className="flex w-full flex-1 flex-col items-start justify-center p-4">
               <TrafficGraph />
+            </div>
+          </div>
+
+          <div className="flex h-full max-w-full flex-col overflow-hidden rounded-xl border border-gray-200 bg-white shadow-sm">
+            <div className="border-b border-gray-200 px-4 py-4">
+              <h2 className="text-lg font-semibold text-gray-800">
+                주요 탐지 시그니처
+              </h2>
+            </div>
+            <div className="flex w-full flex-1 flex-col items-start justify-center p-4">
+              <WordCloud />
             </div>
           </div>
         </div>
