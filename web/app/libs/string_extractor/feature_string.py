9af94a33b6c9a03062530decdbaa35d9e3786699
xorud127 <xorud127@kookmin.ac.kr>
2024-05-18 17:33:33 +0900

    fix: signature

--- web/app/libs/string_extractor/feature_string.py
+++ web/app/libs/string_extractor/feature_string.py
@@ -2,23 +2,16 @@ import os
 import re
 import sys
 import json
+import pickle
 
 # 수정 되어
-SIGNATURES = {'Delete', 'ExitProcess', 'Exception', 'System', 'GetProcAddress', 'LoadLibraryA', 'user32',
-              'advapi32', 'RegCloseKey', 'CloseHandle', '!This program cannot be run in DOS mode.', 'kernel32',
-              'WriteFile', 'VirtualFree', 'VirtualAlloc', 'gram must be run ', 'GetCurrentThread', 'GetLastError',
-              'oleaut32.dll', 'ReadFile', 'KERNEL32.DLL', 'USER32', 'GetFileSize', 'FindClose', 'CharNextA',
-              'SetFilePointer', 'EnterCriticalSection', 'GetFileType', 'TObject', 'CreateFileA', 'RtlUnwind', 'LocalFree',
-              'TlsSetValue', 'WideCharToMultiByte', 'Windows', '.reloc', 'KERNEL32.dll', 'ADVAPI32.dll', '.rdata', '`.data',
-              'StringX', 'CreateDirectoryA', 'ole32.dll', 'Interface', 'MSVBVM60.DLL', 'Boolean', '`.rdat', 'SysFreeString',
-              'GetOEMCP', 'C:\\Program Files\\Microsoft Visual Studio\\VB98\\VB6.OLB', 'UpdateResourceA', 'September',
-              'version', '[[[[[[', '~ExC[)', '{&8p^)j6', 'LookupPrivilegeValueA', 'result', 'MSVCRT.dll', 'MM/dd/yy',
-              'PathFileExistsA', 's`)L$4', 'RtlMoveMemory', 't$t#t$l', 'shlwapi', '! 6J[[', 'lstrcmpiA', '__vbaStrCopy',
-              'nKB\\lll', '<<<<<<<<<<<<<<<<<<<<<<<<<<u9l', '[fPFMlllll', '\\HSplit', 'VC20XC00', 'Dvn+|Ax', 'class_main',
-              '8x1Mjc<', 'FillRect', '2 x|2 ', 'T!pNJBN4', 'u_:o 8', '. T:At', '8s9S=A', 'invalid window size', '[hU-|x&Q `',
-              ':.datu', 'proggam', '"M#m#F', '.pas7k', 'This instance will close.', 'Rich(!', 't"SS9] u', '/scripts/inst_VER103.gz',
-              'x@@P4`/!', 'hitechcell.ca', 'InternetConnectW', 'DefWindowProcW'}
+SIGNATURES = []
 
+pickle_file_path = r"web\app\libs\string_extractor\sig_counter.pkl"
+
+with open(pickle_file_path, "rb") as f:
+    additional_signatures = pickle.load(f)
+    SIGNATURES += additional_signatures
 
 # string feature extract
 def extract_string(path, min_bytes=6):
